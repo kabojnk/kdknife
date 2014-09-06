ea16787ee5f3de40d35590511d479fa0cc3e470a
Kevin Mahoney <kabojnk@gmail.com>
2014-09-06 12:21:51 -0700

    Finished kdknife import script. mongodb storage now no longer stores items in arrays.

--- kdknife.js
+++ kdknife.js
@@ -1,21 +1,53 @@
+// Node libraries
 var fs = require('fs');
 var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
+var parser = new xml2js.Parser({attrkey: '@', explicitArray: false});
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+var jsonfile = require('jsonfile');
 
+// Attempt to connect to the database...
+dbConfig = jsonfile.readFileSync("dbconfig.json");
+
+if (!dbConfig) {
+	throw new Error("Unable to load dbconfig.json");
+	return;
+}
+
+
+
+// Load kanjidic2 XML file...
 var xml = fs.readFileSync('data/kanjidic2.xml', 'utf8');
 
+// Parse the XML...
+console.log("Parsing XML...")
 parser.parseString(xml, function(err, result){
 	if (err) {
-		console.log(err);
+		throw err;
 	}
-	if (result.kanjidic2.character != null) {
-		result.kanjidic2.character.forEach(function(element, index, array){
-			console.dir(element);		
-		});
+	
+	// Setup the mongodb URI for connecting...
+	var mongoUri = 'mongodb://';
+	if (dbConfig.username && dbConfig.username.length) {
+		mongoUri += dbConfig.username + '@' + dbConfig.password;
 	}
-	//console.dir(result);
-	//console.log(util.inspect(result, false, null));
-	console.log('Done');
+	mongoUri += dbConfig.host+':'+dbConfig.port+'/'+dbConfig.database;
+
+	// Connect to the database...
+	MongoClient.connect(mongoUri, function(err, db) {
+    if(err) throw err;
+
+    // If any characters found, start inserting...
+    if (result.kanjidic2.character != null) {
+			result.kanjidic2.character.forEach(function(element, index, array){
+				var collection = db.collection('kanji');
+				console.log(format("On %s of %s", index, array.length));
+				collection.insert(element, function(err, docs){
+					if (err) throw err;
+				});
+			});
+		}
+  });
 });
 
 //console.log(xml);
